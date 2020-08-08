4adb87ab6b85903003b997e5ab38ea2c123adfc7
UnoMo <uno@unomo.com>
2020-08-09 00:29:39 +0200

    Upload Photo added

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
+const path = require("path");
 const express = require("express");
 const dotenv = require("dotenv");
 //const logger = require("./middleware/logger");
 const morgan = require("morgan");
 const colors = require("colors");
+const fileupload = require("express-fileupload");
 const errorHandler = require("./middleware/error");
 const connectDB = require("./config/db");
 
@@ -25,6 +27,13 @@ app.use(express.json());
 if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
 }
+
+// File uploading
+app.use(fileupload());
+
+// Set static folder
+app.use(express.static(path.join(__dirname, "public")));
+
 //app.use(logger);
 
 // Mount Routers
