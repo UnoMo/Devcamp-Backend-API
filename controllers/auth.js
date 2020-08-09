7fbd1152c4128d676556e668abf004681004082d
UnoMo <uno@unomo.com>
2020-08-09 11:52:07 +0200

    Added user registration and encrypting passwords

--- controllers/auth.js
+++ controllers/auth.js
@@ -6,7 +6,15 @@ const User = require("../models/User");
 // @route   GET /api/v1/auth
 // @access  Public
 exports.register = asyncHandler(async (req, res, next) => {
-  console.log("Controller auth her......");
+  const { name, email, password, role } = req.body;
+
+  // Create User
+  const user = await User.create({
+    name,
+    email,
+    password,
+    role
+  });
 
   res.status(200).json({ success: true });
 });
